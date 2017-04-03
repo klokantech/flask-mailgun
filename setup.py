ee95b7b20adc9a00e6efa30a1a83d68a89c704f2
Václav Klusák <vaclav.klusak@klokantech.com>
2017-04-03 14:21:26 +0200

    Release 1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Mailgun',
-    version='1.0',
+    version='1.1',
     description='Mailgun integration for Flask',
     py_modules=['flask_mailgun'],
     install_requires=[
